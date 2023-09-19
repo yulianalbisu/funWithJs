a76f69e21bcf92246cf5644f1f335099c0c7630d
yuliana aldrich <yulianaaldrich@yulianas-MacBook-Air.local>
2023-09-19 09:33:31 -0400

    adding animation in CSS, making a function to change colors and tag

--- hexScript.js
+++ hexScript.js
@@ -1,11 +1,17 @@
-let index = 0;
+function hexColor(){
 
-function changeColors(){
-    const colors = ['red', 'blue', 'orange', 'purple', 'green']
+    const hexText = ['0', '1', '2', '3', '4', '5','6', '7','8','9', 'A','B','C','D','E','F'];
+    let hexcode = '';
 
-    document.getElementsByTagName('body')[0].style.background = colors[index++]
+for(let i=0; i < 6; i++)
+{
+    const random_index = Math.floor(Math.random() * hexText.length);
+    hexcode += hexText[random_index]
+}
+console.log(Math.floor(Math.random() * hexText.length))
 
-    if(index > colors.length - 1){
-        index = 0
-    }
+document.getElementById('hex-code').innerHTML = hexcode;
+
+document.getElementsByTagName('body')[0].style.background = '#' + hexcode;
+
 }
